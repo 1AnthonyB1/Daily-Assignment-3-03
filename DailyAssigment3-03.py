5a2f4328a5e5a13cdcf988383f43ec5f680cfb31
1AnthonyB1 <124379293+1AnthonyB1@users.noreply.github.com>
2023-04-18 14:01:13 +0000

    Finished Daily Assigment 3-03

--- DailyAssigment3-03.py
+++ DailyAssigment3-03.py
@@ -1,1 +1,13 @@
+for number in range (10,100):
+    x = (number % 10)
+    y = (number/7)
+    
+    if x == y:
+        print((number),"Correct Number")
+        magicnumber = (number)
+    elif x != y:
+        print((number),"Incorrect Number")
 
+if number == 99:
+    print()
+    print("The correct number is,",(magicnumber))
